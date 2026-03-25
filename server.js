083b484848fc5548afcdd4c8667a11095c86ad5a
mahmoud <mahmodnabelsi@gmail.com>
2026-03-25 02:08:29 +0200

    fix: resolve Express 5 catch-all route causing 503 error on Hostinger deployment

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'dist')));
 
 // A "catchall" handler for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
